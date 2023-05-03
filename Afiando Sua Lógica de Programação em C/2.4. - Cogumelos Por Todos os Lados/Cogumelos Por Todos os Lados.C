48ba03aec23638dc2bd46b352406e0f52173f23b
Erick Bryan Cubas <thecubas@outlook.com>
2023-05-03 02:17:34 -0300

    commit

---
+++ Afiando Sua Lógica de Programação em C/2.4. - Cogumelos Por Todos os Lados/Cogumelos Por Todos os Lados.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int minutos, cogumelosColhidos;
+
+  // Lê a quantidade de minutos disponíveis para colheita
+  scanf("%d", &minutos);
+
+  // TODO: Calcule e imprima a quantidade de cogumelos colhidos.
+
+  return 0;
+}
