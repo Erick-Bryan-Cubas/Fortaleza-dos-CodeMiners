975cc73c05f06e6162a055af616fc8ba06f5f3f9
Erick Bryan Cubas <thecubas@outlook.com>
2023-05-03 02:20:57 -0300

    commit

--- Afiando Sua Lógica de Programação em C/2.4. - Cogumelos Por Todos os Lados/Cogumelos Por Todos os Lados.C
+++ Afiando Sua Lógica de Programação em C/2.4. - Cogumelos Por Todos os Lados/Cogumelos Por Todos os Lados.C
@@ -7,6 +7,8 @@ int main() {
   scanf("%d", &minutos);
 
   // TODO: Calcule e imprima a quantidade de cogumelos colhidos.
+  cogumelosColhidos = minutos / 3;
+  printf("%d", cogumelosColhidos);
 
   return 0;
 }
